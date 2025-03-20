0e9583bca14380a89b1a7a882b0f0528622d9acf
Alexander Schmidtpeter <a.schmidtpeter@cptrd.de>
2025-03-20 15:45:27 +0100

    feat(ui): Added new version

--- apps/app/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import sharedConfig from "@repo/ui/tailwind.config";
-
-const config = {
-  ...sharedConfig,
-  content: ["./app/**/*.{ts,tsx}", "../../packages/ui/ui/**/*.{ts,tsx}"],
-};
-
-export default config;
