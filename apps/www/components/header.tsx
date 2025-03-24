648c5299c15240902fe43d0651f176dde94bddf0
Alexander Schmidtpeter <a.schmidtpeter@cptrd.de>
2025-03-24 17:37:32 +0100

    feat(ui): Added fixed button

--- apps/www/components/header.tsx
+++ apps/www/components/header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { List } from "@phosphor-icons/react";
 import { Button } from "@repo/ui/button";
 import { TypographySpan } from "@repo/ui/text";
 import { useTranslations } from "next-intl";
@@ -14,11 +15,18 @@ const Header = ({ locale }: { locale: string }) => {
   return (
     <>
       <header className="fixed top-8 w-full z-50">
-        <div className="container mx-auto px-[5vw] md:px-6 bg-[#343434]/40 backdrop-blur-[40px] rounded-full py-2 w-[calc(100vw-10vw)] md:w-[calc(100vw-3rem)]">
-          <div className="flex justify-between items-center">
+        <div className="container ml-[5vw] lg:mx-auto px-[5vw] md:px-6 bg-[#343434]/40 backdrop-blur-[40px] rounded-full py-2 w-max lg:w-[calc(100vw-10vw)] md:w-[calc(100vw-3rem)]">
+          <div className="flex justify-start space-x-4 lg:justify-between items-center">
             <NextLink href="/">
-              <span className="text-white">Fix App</span>
+              <span className="text-white">Fix</span>
             </NextLink>
+            <button
+              className="md:hidden mx-auto"
+              onClick={() => setIsMenuOpen(true)}
+            >
+              <List className="text-white" size={24} />
+            </button>
+
             <div className="hidden md:flex gap-8 items-center">
               {navLinks.map((link) => (
                 <NextLink key={link.href} href={link.href}>
@@ -40,17 +48,14 @@ const Header = ({ locale }: { locale: string }) => {
                 </Button>
               </a>
             </div>
-            <Button
-              className="md:hidden"
-              variant="secondary"
-              onClick={() => setIsMenuOpen(true)}
-            >
-              <TypographySpan>
-                {locale === "de" ? "Menü" : "Menu"}
-              </TypographySpan>
-            </Button>
           </div>
         </div>
+        <Button
+          variant="secondary"
+          className="fixed top-8 right-[5vw] lg:hidden"
+        >
+          Report
+        </Button>
       </header>
       <Menu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
     </>
